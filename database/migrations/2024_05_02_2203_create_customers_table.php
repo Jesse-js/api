7e1780baa7a425e84179464eb0b5122cdb409491
Jesse-js <jesse-jjs@hotmail.com>
2024-05-02 22:20:45 -0300

    feat: adds customers table

---
+++ database/migrations/2024_05_02_2203_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once __DIR__ . '/../../Facades/Migration.php';
+
+class CreateCustomersTable extends Migration
+{
+    public function up(): void
+    {
+        $sql = "CREATE TABLE IF NOT EXISTS `customers` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `name` varchar(200) NOT NULL,
+            `gender` CHAR(1) NOT NULL,
+            `date_of_birth` DATE NOT NULL,
+            `state` varchar(50) NOT NULL,
+            `city` varchar(100) NOT NULL,
+            `street` varchar(150) NOT NULL,
+            `number` varchar(10) NOT NULL,
+            `zip_code` varchar(8) NOT NULL,
+            `email` varchar(150) NOT NULL UNIQUE,
+            `telephone` varchar(20) NOT NULL,
+            `document_number` varchar(20) NOT NULL UNIQUE,
+            `document_type` varchar(4) NOT NULL DEFAULT 'cpf',
+            `status` tinyint(1) NOT NULL DEFAULT '1',
+            `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            PRIMARY KEY (`id`)
+        )";
+
+        $this->conn->exec($sql);
+    }
+
+    public function down(): void
+    {
+        $this->conn->exec("DROP TABLE IF EXISTS `customers`");
+    }
+}
